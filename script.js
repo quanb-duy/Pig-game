960357a0064b411b3f72c633f261e5a24f862f4c
quanb-duy <quanbuid@gmail.com>
2024-02-04 22:54:45 +0100

    jdu spat

--- script.js
+++ script.js
@@ -3,23 +3,23 @@
 // selecting elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 50;
+score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const currentScore1 = document.getElementById('current--0');
-const currentScore2 = document.getElementById('current--1');
+let currentScore = 0;
+
 const activePlayer1 = document.querySelector('.player--0');
 const activePlayer2 = document.querySelector('.player--1');
-
-const scoreCounter1 = 0;
-const scoreCounter2 = 0;
 
 function randomDice() {
   const randInt = Math.floor(Math.random() * 6) + 1;
@@ -34,4 +34,13 @@ btnRoll.addEventListener('click', function () {
   // display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  // check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // change later
+  } else {
+    // switch to next player
+  }
 });
